3ac3215c07677e933a475d5a31278fc7863e875b
mhmdtshref <mhmdtshref@gmail.com>
2018-11-06 16:03:28 +0200

    replace-apiToken-method-with-variable #2

--- js/api.js
+++ js/api.js
@@ -1,4 +1,8 @@
-function apiToken() {
-    return "b07c4b76ddb7f8611ad630286170d29c";
-}
+var apiToken = "b07c4b76ddb7f8611ad630286170d29c";
 
+var urlDomain = "https://api.themoviedb.org/";
+
+
+function mostPopular() {
+    fetch(urlDomain).then();
+}
