804e5cd3fcc8bef706e5ab796931fcd7e6af0e75
mhmdtshref <mhmdtshref@gmail.com>
2018-11-07 08:56:17 +0200

    most-popular-added #3

--- js/api.js
+++ js/api.js
@@ -1,8 +1,17 @@
-var apiToken = "b07c4b76ddb7f8611ad630286170d29c";
+var apiToken = "?api_key=b07c4b76ddb7f8611ad630286170d29c";
 
-var urlDomain = "https://api.themoviedb.org/";
+var urlDomain = "https://api.themoviedb.org/3/";
 
 
+// Issue #3
 function mostPopular() {
-    fetch(urlDomain).then();
+    var data = fetch(urlDomain + "movie/popular" + apiToken).then(response => response.json());
+    console.log(data)
 }
+
+
+function addPopularMoviesCards() {
+
+}
+
+
