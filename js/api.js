55e399cf174b4803dc7f2b41379a1dbc659d5861
mhmdtshref <mhmdtshref@gmail.com>
2018-11-07 15:29:17 +0200

    dom-functions-added

--- js/api.js
+++ js/api.js
@@ -2,17 +2,39 @@ var apiToken = "?api_key=b07c4b76ddb7f8611ad630286170d29c";
 
 var urlDomain = "https://api.themoviedb.org/3/";
 
-
 // Issue #3
 function mostPopular() {
-    var data = fetch(urlDomain + "movie/popular" + apiToken).then(response => response.json());
-    console.log(data)
+    var data = fetch(urlDomain + "movie/popular" + apiToken)
+        .then(response => {
+
+            return response.json();
+
+        });
+    // console.log(data);
+    return data;
+
 }
 
+function moviesToCards(title, data) {
+    var  MoviesContainerElement = document.createElement("div");
+    data.then((value)=>{
+        console.log('value',value);
+        var movies = (value.results).forEach(movieData => {
+            // Create html card element from movie argument.
+            var posterUrl = "https://image.tmdb.org/t/p/w600_and_h900_bestv2" + movieData.poster_path;
+            var movieElement = createCardElement(movieData.title, movieData.release_date.substring(0, 4), posterUrl);
+            // Add html card element to MoviesContainerElement.
+            MoviesContainerElement.appendChild(movieElement);
+        });
+        //TODO: Should add element ID.
+        document.getElementById("card-parent-div").innerHTML = "";
+        document.getElementById("card-parent-div").appendChild(MoviesContainerElement);
+    })
 
-function moviesToCards(title, movies) {
 
 }
+//myNode.childNodes = new Array();
+
 // issue #4
 function searchMovie(title, year, adult) {
     var titleQuery = "&query=" + title;
@@ -24,3 +46,44 @@ function searchMovie(title, year, adult) {
     return data;
 }
 
+function createCardElement(title, year, posterUrl) {
+    var card = document.createElement("div");
+    card.innerHTML = "<h1>" + title + " (" + year + ")" + "</h1><img src='" + posterUrl + "'>";
+    //document.getElementById("myDIV").appendChild(card);
+    return card;
+}
+
+function sortNodeList(sortBy, nodes) {
+    var nodeArray = [];
+    switch(sortBy) {
+        case 'AZ':
+            nodeArray =  (Array.from(nodes)).sort((a, b) => {
+                if(a.title < b.title) { return -1; }
+                if(a.title > b.title) { return 1; }
+                return 0;
+            });
+            //return
+            break;
+        case 'year':
+            nodeArray = (Array.from(nodes)).sort((a, b) => a.year - b.year);
+            break;
+        case 'rate':
+            nodeArray = (Array.from(nodes)).sort((a, b) => a.vote_avarage - b.vote_avarage);
+            break;
+        default: return 'error';
+    }
+    return nodeArray;
+}
+
+function nodeArrayToCards(nodeArray){
+    var section = document.getElementById("");
+
+    nodeArray.forEach(node => {
+        section.appendChild(node);
+    });
+}
+
+function moviesSectionNodes(){
+    return document.getElementById("card-parent-div");
+
+}
